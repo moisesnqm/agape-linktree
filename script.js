3cb66fe922840b6b9fef092adec19ff2b7e4fa9a
Moises Machado <moisesnqm@hotmail.com>
2021-11-10 01:01:07 -0300

    edição produção

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
+var alertList = document.querySelectorAll('.alert')
+var alerts =  [].slice.call(alertList).map(function (element) {
+  return new bootstrap.Alert(element)
+})
 var alertPlaceholder = document.getElementById('liveAlertPlaceholder')
 var alertTrigger = document.getElementById('liveAlertBtn')
+const type = 'danger'
+const message = 'Em breve'
 
-function alert(message, type) {
-  var wrapper = document.createElement('div')
-  wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
-
-  alertPlaceholder.append(wrapper)
+function alert() {
+  var wrapper = document.getElementById('liveAlertPlaceholder')
+  wrapper.innerHTML = '<div class="alert alert-warning alert-dismissible fade show" role="alert"><strong>Desculpe!</strong> Essa página ainda não está disponível.<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
+  setTimeout(function() {document.getElementById('liveAlertPlaceholder').innerHTML='';},3000);
 }
 
-if (alertTrigger) {
-  alertTrigger.addEventListener('click', function () {
-    alert('Nice, you triggered this alert message!', 'success')
-  })
-}
+document.addEventListener("DOMContentLoaded", function() {
+  var conteudo = 'Acesse os links fáceis do Ágape! - http://agape.org.br/links';
+  document.getElementById("whatsapp-share-btt").href = "https://api.whatsapp.com/send?text=" + conteudo;
+}, false);
