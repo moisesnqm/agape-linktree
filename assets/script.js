8d9c0473779eb360c55f36a0aa672470566100e5
Moises Machado <moisesnqm@hotmail.com>
2021-11-10 02:13:07 -0300

    bug fix

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+var alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+var alertTrigger = document.getElementById('liveAlertBtn')
+
+function alert() {
+  var wrapper = document.getElementById('liveAlertPlaceholder')
+  wrapper.innerHTML = '<div class="alert alert-warning alert-dismissible fade show" role="alert"><strong>Desculpe!</strong> Essa página ainda não está disponível.<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
+  setTimeout(function() {document.getElementById('liveAlertPlaceholder').innerHTML='';},3000);
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  var conteudo = 'Acesse os links fáceis do Ágape! - http://agape.org.br/links';
+  document.getElementById("whatsapp-share-btt").href = "https://api.whatsapp.com/send?text=" + conteudo;
+}, false);
